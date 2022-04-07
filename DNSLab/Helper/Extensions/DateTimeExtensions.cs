cf989857bc825456df138de8f7655d3170500b4c
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-04-07 11:46:58 +0430

    Add DateTimeExtensions Change DNSHistoryDateTimes

---
+++ DNSLab/Helper/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using DNSLab.Helper.Utilities;
+using MD.PersianDateTime.Standard;
+using System.Globalization;
+
+namespace DNSLab.Helper.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string CalcRelativeTime(this DateTime dateTime)
+        {
+           return RelativeTimeCalculator.Calc(dateTime);
+        }
+
+        public static string ToLocalizerString(this DateTime dateTime)
+        {
+            DateTime localDateTime = dateTime.ToLocalTime();
+
+            switch (CultureInfo.CurrentCulture.Name)
+            {
+                case "fa-FA":
+                    return new PersianDateTime(localDateTime).ToString();
+                case "en-EN":
+                default:
+                    return localDateTime.ToString();
+            }
+            
+
+        }
+    }
+}
