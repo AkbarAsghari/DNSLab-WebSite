b494a7c7127decbb681b0b73e3fd7dc72d67765a
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-04-24 22:59:35 +0430

    Add Change Password Form Change User Info UI

--- DNSLab/DTOs/User/ChangePasswordDTO.cs
+++ DNSLab/DTOs/User/ChangePasswordDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,15 @@ namespace DNSLab.DTOs.User
 {
     public class ChangePasswordDTO
     {
+        [Required]
+        [DataType(DataType.Password)]
         public string CurrentPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword")]
+        public string ConfirmNewPassword { get; set; }
     }
 }
