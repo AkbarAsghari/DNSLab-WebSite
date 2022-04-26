bd2e9973ebc66386323c96b3b23519b256f34723
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-04-26 23:13:38 +0430

    Add Comment Repository

--- DNSLab/Interfaces/Repository/ICommentRepository.cs
+++ DNSLab/Interfaces/Repository/ICommentRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace DNSLab.Interfaces.Repository
+﻿using DNSLab.DTOs.Comments;
+
+namespace DNSLab.Interfaces.Repository
 {
     public interface ICommentRepository
     {
-
+        Task<bool> AddNewComment(CreateCommentDTO comment);
+        Task<CommentDTO> GetComment(Guid id);
+        Task<IEnumerable<CommentDTO>> GetMyComments();
+        Task<IEnumerable<FullCommentDTO>> GetAllComments();
+        Task<bool> RemoveComment(Guid id);
+        Task<bool> UpdateComment(CommentDTO comment);
     }
 }
--- DNSLab/Repository/CommentRepository.cs
+++ DNSLab/Repository/CommentRepository.cs
@@ -1,6 +1,51 @@
-﻿namespace DNSLab.Repository
+﻿using DNSLab.DTOs.Comments;
+using DNSLab.Interfaces.Helper;
+using DNSLab.Interfaces.Repository;
+
+namespace DNSLab.Repository
 {
-    public class CommentRepository
+    public class CommentRepository : ICommentRepository
     {
+        private readonly IHttpService _httpService;
+        public CommentRepository(IHttpService httpService)
+        {
+            this._httpService = httpService;
+        }
+
+        public async Task<bool> AddNewComment(CreateCommentDTO comment)
+        {
+            var result = await _httpService.Post<CreateCommentDTO, bool>($"/Comment/Create", comment);
+            return result.Response;
+        }
+
+        public async Task<IEnumerable<FullCommentDTO>> GetAllComments()
+        {
+            var result = await _httpService.Get<IEnumerable<FullCommentDTO>>($"/Comment/GetAllComments");
+            return result.Response;
+        }
+
+        public async Task<CommentDTO> GetComment(Guid id)
+        {
+            var result = await _httpService.Get<CommentDTO>($"/Comment/get?Id={id}");
+            return result.Response;
+        }
+
+        public async Task<IEnumerable<CommentDTO>> GetMyComments()
+        {
+            var result = await _httpService.Get<IEnumerable<FullCommentDTO>>($"/Comment/GetMyComments");
+            return result.Response;
+        }
+
+        public async Task<bool> RemoveComment(Guid id)
+        {
+            var result = await _httpService.Delete<bool>($"/Comment/Delete?Id={id}");
+            return result.Response;
+        }
+
+        public async Task<bool> UpdateComment(CommentDTO comment)
+        {
+            var result = await _httpService.Put<CommentDTO, bool>($"/Comment/Update", comment);
+            return result.Response;
+        }
     }
 }
