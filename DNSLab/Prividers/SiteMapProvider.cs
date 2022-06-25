e7dc126f436b700b044514617d02ec70861e9691
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-06-26 00:49:32 +0430

    Add Excepted Pages for site map Remove blog route

--- DNSLab/Prividers/SiteMapProvider.cs
+++ DNSLab/Prividers/SiteMapProvider.cs
@@ -6,16 +6,28 @@ namespace DNSLab.Prividers
 {
     public class SiteMapProvider
     {
+        private static string[] _ExceptedPages = new string[]
+        {
+            "https://dnslab.ir/user/confirmemail/{token}",
+            "https://dnslab.ir/user/resetpassword/{token}",
+            "https://dnslab.ir/404",
+            "https://dnslab.ir/500",
+            "https://dnslab.ir/503",
+            "https://dnslab.ir/user/logout",
+        };
+
         public static async Task Generate(HttpContext context)
         {
             var pages = Assembly.GetExecutingAssembly().ExportedTypes.Where(p =>
                 p.IsSubclassOf(typeof(ComponentBase)));
 
             foreach (var route in pages.Where(x => x.CustomAttributes.Any(x => x.AttributeType == typeof(RouteAttribute)) &&
-                                                   !x.CustomAttributes.Any(x=>x.AttributeType == typeof(AuthorizeAttribute)))
+                                                   !x.CustomAttributes.Any(x => x.AttributeType == typeof(AuthorizeAttribute)))
                 .Select(x => x.CustomAttributes.FirstOrDefault(x => x.AttributeType == typeof(RouteAttribute))!.ConstructorArguments[0].Value))
             {
-                await context.Response.WriteAsync("https://dnslab.ir" + route + "\n");
+                string routeWithDomain = $"https://dnslab.ir{route}";
+                if (!_ExceptedPages.Contains(routeWithDomain.ToLower()))
+                    await context.Response.WriteAsync(routeWithDomain + "\n");
             }
         }
     }
