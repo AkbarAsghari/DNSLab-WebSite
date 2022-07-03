bb23b75323bc3394cc1b89ca106b41c0da373946
Akbar Asghari <AkbarAsghari@Outlook.com>
2022-07-03 23:04:35 +0430

    Fix CKEditor

--- DNSLab/DTOs/Pages/CreatePageDTO.cs
+++ DNSLab/DTOs/Pages/CreatePageDTO.cs
@@ -11,7 +11,7 @@ namespace DNSLab.DTOs.Pages
         public string URL { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string[] Keywords { get; set; }
+        public List<string> Keywords { get; set; }
         public string Header { get; set; }
         public string Body { get; set; }
     }
