95c8a152159cb7e99833d10fe675711793ec8936
Akbar Asghari <akbarasghari@outlook.com>
2022-08-16 23:38:58 +0430

    Add SiteChangesDTO DTO

---
+++ DNSLab/DTOs/Statics/SiteChangesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSLab.DTOs.Statics
+{
+    public class SiteChangesDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string InformationLink { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
