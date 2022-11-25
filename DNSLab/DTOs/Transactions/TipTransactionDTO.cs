002fa3c02d5add7ca705d7352540fb62eb022ccb
Akbar Asghari <akbarasghari@outlook.com>
2022-11-25 22:38:52 +0330

    Add Tip UI

---
+++ DNSLab/DTOs/Transactions/TipTransactionDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSLab.DTOs.Transactions
+{
+    public class TipTransactionDTO
+    {
+        public Guid? UserId { get; set; }
+        public string? FullName { get; set; }
+        public string? Mobile { get; set; }
+        public int Amount { get; set; }
+        public string? Description { get; set; }
+        public string? OrderId { get; set; }
+    }
+}
