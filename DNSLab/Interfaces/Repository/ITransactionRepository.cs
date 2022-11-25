78c11a5733067690ad103dc73369240fb0650e54
Akbar Asghari <akbarasghari@outlook.com>
2022-11-25 22:51:04 +0330

    Add Transaction Repository Implement Tip Method

---
+++ DNSLab/Interfaces/Repository/ITransactionRepository.cs
@@ -0,0 +1,10 @@
+﻿using DNSLab.DTOs.Transactions;
+
+namespace DNSLab.Interfaces.Repository
+{
+    public interface ITransactionRepository
+    {
+        Task<string> Tip(TipTransactionDTO request);
+        Task<bool> Verify(long trackId);
+    }
+}
---
+++ DNSLab/Repository/TransactionRepository.cs
@@ -0,0 +1,29 @@
+﻿using DNSLab.DTOs.Transactions;
+using DNSLab.Interfaces.Helper;
+using DNSLab.Interfaces.Repository;
+
+namespace DNSLab.Repository
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly IHttpService _httpService;
+        public TransactionRepository(IHttpService httpService)
+        {
+            this._httpService = httpService;
+        }
+
+        public async Task<string> Tip(TipTransactionDTO request)
+        {
+            var response = await _httpService.Post<TipTransactionDTO, string>($"/Transaction/Tip", request);
+            if (!response.Success)
+                return String.Empty;
+            else
+                return response.Response;
+        }
+
+        public Task<bool> Verify(long trackId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
