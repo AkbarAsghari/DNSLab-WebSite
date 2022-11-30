ae0deab61cc11a4e6e1dc7802fba76ecfb83b425
Akbar Asghari <akbarasghari@outlook.com>
2022-11-30 23:31:24 +0330

    Make Copy To Clipboard Optimize

--- DNSLab/wwwroot/js/custom/CopyToClipboard.js
+++ DNSLab/wwwroot/js/custom/CopyToClipboard.js
@@ -8,18 +8,3 @@
         return true;
     }
 };
-
-function addTooltips(toolTipId) {
-    $("#" + toolTipId).tooltip({
-        delay: { show: 0, hide: 1200 },
-        title: 'برای کپی کردن کلیک کنید'
-    });
-
-    $("#" + toolTipId).on('click', function () {
-        var tt = bootstrap.Tooltip.getInstance(this);
-        tt.dispose();
-        this.setAttribute('title', 'کپی شد')
-        tt = bootstrap.Tooltip.getOrCreateInstance(this);
-        tt.show();
-    });
-}
