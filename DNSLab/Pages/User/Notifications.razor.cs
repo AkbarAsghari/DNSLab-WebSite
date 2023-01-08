127824378b9f5ccddb7f6d9e41b89a1dc1997dda
Akbar Asghari <akbarasghari@outlook.com>
2023-01-09 00:35:46 +0330

    add Notification Logics

---
+++ DNSLab/Pages/User/Notifications.razor.cs
@@ -0,0 +1,23 @@
+﻿using DNSLab.DTOs.User;
+
+namespace DNSLab.Pages.User;
+
+partial class Notifications
+{
+    private UserInfo userInfo;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            userInfo = await _AccountRepository.Get();
+            await this.InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    private async Task SaveChanges()
+    {
+        if (await _AccountRepository.Update(userInfo))
+            _ToastService.ShowToast("تغییرات ذخیره شد", Enums.ToastLevel.Success);
+    }
+}
