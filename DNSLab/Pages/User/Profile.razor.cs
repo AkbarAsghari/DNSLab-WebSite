a13c69e05e09e93a8b8ec1b304591558c9000e10
Akbar Asghari <akbarasghari@outlook.com>
2023-01-09 00:50:35 +0330

    Change Profile UI

--- DNSLab/Pages/User/Profile.razor.cs
+++ DNSLab/Pages/User/Profile.razor.cs
@@ -4,20 +4,14 @@ namespace DNSLab.Pages.User;
 partial class Profile
 {
     UserInfo userInfo;
-    ChangeEmailDTO changeEmail;
 
     CultureSelector cultureSelector;
-    private bool ResentButtonClicked = false;
-
-    Modal ChangeEmailModal { get; set; }
-
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
             userInfo = await accountRepository.Get();
-            changeEmail = new ChangeEmailDTO { Email = userInfo.Email };
             await this.InvokeAsync(() => StateHasChanged());
         }
     }
@@ -30,37 +24,5 @@ partial class Profile
         }
 
         cultureSelector.Save();
-    }
-
-    private async Task ResendActivationLink()
-    {
-        if (await accountRepository.ResendConfirmEmailToken())
-        {
-            ResentButtonClicked = true;
-        }
-    }
-
-    private async Task OpenChangeEmailModal()
-    {
-        await ChangeEmailModal.Open();
-    }
-
-    private async Task AcceptChangeEmail()
-    {
-        if (await accountRepository.ChangeEmail(changeEmail))
-        {
-            toastService.ShowToast("ایمیل جدید ذخیره شد و ایمیل تایید ارسال گردید", Enums.ToastLevel.Success);
-
-            if (userInfo.Email.Trim().ToLower() != changeEmail.Email.Trim().ToLower())
-                userInfo.IsEmailApproved = false;
-
-            userInfo.Email = changeEmail.Email;
-            await ChangeEmailModal.Close();
-        }
-    }
-
-    async Task DeleteAccount()
-    {
-
     }
 }
