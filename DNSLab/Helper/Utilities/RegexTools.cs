dd0df22887442417434d7be7a0cb755f132f42eb
Akbar Asghari <akbarasghari@outlook.com>
2023-01-21 22:43:27 +0330

    Add Regex For Extract Urls From String

---
+++ DNSLab/Helper/Utilities/RegexTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DNSLab.Helper.Utilities
+{
+    public static class RegexTools
+    {
+        public static IEnumerable<string> ExtractUrlsFromString(string input)
+        {
+            var matches = new Regex(@"(ftp:\/\/|www\.|https?:\/\/){1}[a-zA-Z0-9u00a1-\uffff0-]{2,}\.[a-zA-Z0-9u00a1-\uffff0-]{2,}(\S*)").Matches(input);
+
+            return matches.Select(x => x.Value);
+        }
+    }
+}
