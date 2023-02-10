2a2cfff316f78f275c8c735246186af30576c3f1
Akbar Asghari <akbarasghari@outlook.com>
2023-02-10 11:03:44 +0330

    Translate Persian numbers to english in HostNameDTO

--- DNSLab/DTOs/DNS/CreateHostNameDTO.cs
+++ DNSLab/DTOs/DNS/CreateHostNameDTO.cs
@@ -10,10 +10,54 @@ namespace DNSLab.DTOs.DNS
     {
         public string Name { get; set; }
         public string Domain { get; set; }
-        public string IPv4Address { get; set; }
-        public string IPv6Address { get; set; }
-        public string HostNameAlias { get; set; }
-        public string URLOrIp { get; set; }
+        string _IPv4Address;
+        public string IPv4Address
+        {
+            get
+            {
+                return _IPv4Address;
+            }
+            set
+            {
+                _IPv4Address = value.PersianToEnglishNumbers();
+            }
+        }
+        string _IPv6Address;
+        public string IPv6Address
+        {
+            get
+            {
+                return _IPv6Address;
+            }
+            set
+            {
+                _IPv6Address = value.PersianToEnglishNumbers();
+            }
+        }
+        string _HostNameAlias;
+        public string HostNameAlias
+        {
+            get
+            {
+                return _HostNameAlias;
+            }
+            set
+            {
+                _HostNameAlias = value.PersianToEnglishNumbers();
+            }
+        }
+        string _URLOrIp;
+        public string URLOrIp
+        {
+            get
+            {
+                return _URLOrIp;
+            }
+            set
+            {
+                _URLOrIp = value.PersianToEnglishNumbers();
+            }
+        }
         public int? RedirectHttpResponseStatusCode { get; set; }
         public int RecordType { get; set; }
     }
