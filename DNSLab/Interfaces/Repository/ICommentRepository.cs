d85b4eba89f7899ea90491ec416090d5018d3574
Akbar Asghari <akbarasghari@outlook.com>
2023-02-11 14:00:15 +0330

    Remove extra methods from comment repository

--- DNSLab/Interfaces/Repository/ICommentRepository.cs
+++ DNSLab/Interfaces/Repository/ICommentRepository.cs
@@ -4,17 +4,6 @@ namespace DNSLab.Interfaces.Repository
 {
     public interface ICommentRepository
     {
-        Task<bool> AddNewComment(CreateCommentDTO comment);
-        Task<CommentDTO> GetComment(Guid id);
-        Task<IEnumerable<FullCommentDTO>> GetMyComments();
-        Task<IEnumerable<CommentDTO>> GetNewComments();
-        Task<IEnumerable<FullCommentDTO>> GetAllComments();
-        Task<IEnumerable<FullCommentDTO>> GetCommentReplies(Guid id);
-        Task<int> GetCommentsNotSeenRepliesCount();
-        Task<bool> DeleteComment(Guid id);
-        Task<bool> UpdateComment(CommentDTO comment);
-        Task<bool> ReviewComment(Guid id, bool isApproved);
-        //Page
         Task<bool> AddNewPageComment(CreatePageCommentDTO pageComment);
         Task<bool> RemovePageComment(Guid commentId);
         Task<IEnumerable<PageCommentAndRepliesDTO>> GetPageComments(Guid pageId);
