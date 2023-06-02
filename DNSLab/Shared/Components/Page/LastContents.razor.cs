d9c0d57fbda6e060b0a2633d503cc2888729349b
Akbar Asghari <akbarasghari@outlook.com>
2023-06-02 12:00:06 +0330

    Update DNS Engine From Powershell to Technitium

---
+++ DNSLab/Shared/Components/Page/LastContents.razor.cs
@@ -0,0 +1,11 @@
+﻿using DNSLab.DTOs.Pages;
+
+namespace DNSLab.Shared.Components.Page;
+partial class LastContents
+{
+    IEnumerable<LastContentsDTO> lastContents { get;set; }
+    protected override async Task OnInitializedAsync()
+    {
+        lastContents = await _PageRepository.GetLastContents();
+    }
+}
