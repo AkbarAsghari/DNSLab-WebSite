b13fda51aeb37632586658a4ba3742612c2997b9
Akbar Asghari <akbarasghari@outlook.com>
2023-06-23 02:18:29 +0330

    Remove Old TimeLine Fix DNSChangeHistory UI Fix SiteChangeHistory UI

--- DNSLab/Shared/TimeLine.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DNSLab.Shared;
-partial class TimeLine<TItem>
-{
-    [Parameter] public string TimeLineHeader { get; set; }
-    [Parameter] public IEnumerable<TItem> Data { get; set; }
-    [Parameter] public RenderFragment<TItem> TimeLineItemRenderFragment { get; set; }
-}
--- DNSLab/Shared/TimeLineItem.razor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DNSLab.Shared;
-partial class TimeLineItem
-{
-    [Parameter] public string? Header { get; set; }
-    [Parameter] public RenderFragment Body { get; set; }
-    [Parameter] public bool ShowBodyWithAccordion { get; set; } = false;
-    [Parameter] public DateTime? DateTime { get; set; }
-}
