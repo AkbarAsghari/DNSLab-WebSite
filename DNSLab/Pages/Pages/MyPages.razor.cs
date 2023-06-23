bb2a38e2a2bcaa7aa9342121c0675353807de627
Akbar Asghari <akbarasghari@outlook.com>
2023-06-23 15:17:12 +0330

    Fix MyPages UI

--- DNSLab/Pages/Pages/MyPages.razor.cs
+++ DNSLab/Pages/Pages/MyPages.razor.cs
@@ -1,13 +1,14 @@
 ﻿using DNSLab.DTOs.Pages;
+using DNSLab.Repository;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MudBlazor;
 
 namespace DNSLab.Pages.Pages;
 partial class MyPages
 {
-    IEnumerable<PageSummaryDTO> pageSummaries;
+    [Inject] private IDialogService DialogService { get; set; }
 
-    Modal pageModal { get; set; }
-    bool modalForDeletePage { get; set; }
-    PageSummaryDTO modalRecord { get; set; } = new PageSummaryDTO();
+    IEnumerable<PageSummaryDTO> pageSummaries;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -23,45 +24,37 @@ partial class MyPages
         pageSummaries = await _PageRepository.GetAllPagesSummary();
     }
 
-    private async Task AcceptDelete()
-    {
-        if (modalRecord != null)
-        {
-            if (await _PageRepository.DeletePage(modalRecord.Id))
-            {
-                await pageModal.Close();
-                pageSummaries = pageSummaries.Where(x => x.Id != modalRecord.Id).ToList();
-            }
-        }
-        modalRecord = new PageSummaryDTO();
-    }
-
-    private async Task AcceptPublish()
-    {
-        if (modalRecord != null)
-        {
-            if (await _PageRepository.PublishPage(modalRecord.Id))
-            {
-                await pageModal.Close();
-                pageSummaries.First(x => x.Id == modalRecord.Id).IsPublieshed = true;
-                Snackbar.Add(localizer["PublishedSuccess"], MudBlazor.Severity.Success);
-            }
-        }
-        modalRecord = new PageSummaryDTO();
-    }
-
     private async Task DeletePage(PageSummaryDTO record)
     {
-        modalForDeletePage = true;
-        modalRecord = record;
-        await pageModal.Open();
+        bool? result = await DialogService.ShowMessageBox(
+           "هشدار",
+           $"آیا از حذف صفحه {record.Title} مطمئن هستید؟",
+           yesText: "حذف", cancelText: "انصراف");
+
+        if (result == true)
+        {
+            if (await _PageRepository.DeletePage(record.Id))
+            {
+                pageSummaries = pageSummaries.Where(x => x.Id != record.Id).ToList();
+            }
+        }
     }
 
     private async Task PublishPage(PageSummaryDTO record)
     {
-        modalForDeletePage = false;
-        modalRecord = record;
-        await pageModal.Open();
+        bool? result = await DialogService.ShowMessageBox(
+           "هشدار",
+           $"آیا از انتشار صفحه {record.Title} مطمئن هستید؟",
+           yesText: "انتشار", cancelText: "انصراف");
+
+        if (result == true)
+        {
+            if (await _PageRepository.PublishPage(record.Id))
+            {
+                pageSummaries.First(x => x.Id == record.Id).IsPublieshed = true;
+                Snackbar.Add(localizer["PublishedSuccess"], MudBlazor.Severity.Success);
+            }
+        }
     }
 
     private async Task EditPage(PageSummaryDTO record)
