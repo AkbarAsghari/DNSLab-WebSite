c01c18a0f9acc0fd49e95ed01f6a61a330932f01
Akbar Asghari <akbarasghari@outlook.com>
2023-06-23 14:23:17 +0330

    Fix Comment Detail Page

---
+++ DNSLab/Helper/Utilities/ProfileImageCreator.cs
@@ -0,0 +1,12 @@
+﻿using Jdenticon;
+
+namespace DNSLab.Helper.Utilities
+{
+    public class ProfileImageCreator
+    {
+        public static string GenerateSVG(Guid userId,int size = 0)
+        {
+            return Identicon.FromValue(userId, size: size == 0 ? 100 : size).ToSvg();
+        }
+    }
+}
