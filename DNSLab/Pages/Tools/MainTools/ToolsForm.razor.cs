c60f7e15f26a283789689a8e24f1ad61e72ac926
Akbar Asghari <akbarasghari@outlook.com>
2023-06-26 22:43:11 +0330

    Remove All Cards and Make Simple

--- DNSLab/Pages/Tools/MainTools/ToolsForm.razor.cs
+++ DNSLab/Pages/Tools/MainTools/ToolsForm.razor.cs
@@ -4,7 +4,7 @@ partial class ToolsForm<TItem>
 {
     [Parameter] public TItem Model { get; set; }
     [Parameter] public string Header { get; set; }
-    [Parameter] public RenderFragment Body { get; set; }
+    [Parameter] public RenderFragment ChildContent { get; set; }
     [Parameter] public EventCallback OnValidSubmit { get; set; }
     [Parameter] public List<string> Results { get; set; }
     [Parameter] public bool IsProgressing { get; set; }
