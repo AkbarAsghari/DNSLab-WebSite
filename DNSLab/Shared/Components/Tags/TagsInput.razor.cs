af333b116cce234434061c4cf697de7e52901513
Akbar Asghari <akbarasghari@outlook.com>
2023-07-03 16:37:06 +0000

    Fix UI of Tag

--- DNSLab/Shared/Components/Tags/TagsInput.razor.cs
+++ DNSLab/Shared/Components/Tags/TagsInput.razor.cs
@@ -5,38 +5,22 @@ namespace DNSLab.Shared.Components.Tags;
 partial class TagsInput
 {
     string input;
-    protected string Value
+    public void SubmitTag()
     {
-        get
+        if (Tags == null)
+            Tags = new List<string>();
+
+        IsContainSpecialCharacter = false;
+
+        if (!String.IsNullOrEmpty(input)
+        && !Tags.Exists(t => t.Equals(input, StringComparison.CurrentCultureIgnoreCase)))
         {
-            return input;
+            Tags.Add(input);
         }
-        set
-        {
-            if (Tags == null)
-                Tags = new List<string>();
 
-            IsContainSpecialCharacter = false;
-
-            if (!String.IsNullOrEmpty(value))
-            {
-                if (value.EndsWith(",") || value.EndsWith("،"))
-                {
-                    value = Value.Replace(",", String.Empty).Replace("،", String.Empty).Trim();
-
-                    if (!String.IsNullOrEmpty(value)
-                    && !Tags.Exists(t => t.Equals(value, StringComparison.CurrentCultureIgnoreCase)))
-                    {
-                        Tags.Add(value);
-                    }
-
-                    value = string.Empty;
-                }
-            }
-
-            input = value ?? string.Empty;
-        }
+        input = String.Empty;
     }
+    
     protected bool IsContainSpecialCharacter { get; set; }
     [Parameter] public List<string> Tags { get; set; }
 
