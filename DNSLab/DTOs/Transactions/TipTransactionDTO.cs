332cdfc4cc266afd732531df41291566ef73c7ae
Akbar Asghari <akbarasghari@outlook.com>
2023-07-28 16:42:02 +0330

    Add Payment Page

--- DNSLab/DTOs/Transactions/TipTransactionDTO.cs
+++ DNSLab/DTOs/Transactions/TipTransactionDTO.cs
@@ -8,10 +8,6 @@ namespace DNSLab.DTOs.Transactions
 {
     public class TipTransactionDTO
     {
-        public Guid? UserId { get; set; }
-        public string? FullName { get; set; }
-        public string? Mobile { get; set; }
-        public int Amount { get; set; }
-        public string? OrderId { get; set; }
+        public int Amount { get; set; } = 50000;
     }
 }
---
+++ DNSLab/Shared/SupportUs/TipPayment.razor.cs
@@ -0,0 +1,19 @@
+﻿using DNSLab.DTOs.Transactions;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace DNSLab.Shared.SupportUs;
+
+partial class TipPayment
+{
+    public TipTransactionDTO TipTransaction { get; set; } = new TipTransactionDTO { };
+
+    public IMask amountMask = new RegexMask(@"^\d{0,8}$");
+
+    private async Task Go()
+    {
+        var result = await _TransactionRepository.Tip(TipTransaction);
+        if (!String.IsNullOrEmpty(result))
+            navigationManager.NavigateTo(result);
+    }
+}
