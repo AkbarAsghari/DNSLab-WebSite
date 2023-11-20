2b9b6c127c3db64cee5d76c1e9292ea57ff00760
Akbar Asghari <akbarasghari@outlook.com>
2023-11-20 23:12:10 +0330

    Some UI Change for Subscriptions Plan

--- DNSLab/Pages/Subscriptions/Plans.razor.cs
+++ DNSLab/Pages/Subscriptions/Plans.razor.cs
@@ -1,15 +1,31 @@
 ﻿using DNSLab.DTOs.Subscriptions;
 using DNSLab.Interfaces.Repository;
+using Microsoft.AspNetCore.Components;
 
 namespace DNSLab.Pages.Subscriptions;
 
 partial class Plans
 {
     [Inject] ISubscriptionsRepository _SubscriptionsRepository { get; set; }
+    [Inject] NavigationManager navigationManager { get; set; }
 
     IEnumerable<PlanInfoDTO> PlansInfo;
     protected override async Task OnInitializedAsync()
     {
         PlansInfo = await _SubscriptionsRepository.GetPlans();
+    }
+
+    bool _DisabledBuyButton = false;
+    async Task Buy(int id)
+    {
+        _DisabledBuyButton = true;
+        var result = await _SubscriptionsRepository.Subscription(new DTOs.Transactions.SubscriptionTransactionDTO
+        {
+            Plan = id
+        });
+
+        if (!String.IsNullOrEmpty(result))
+            navigationManager.NavigateTo(result);
+        _DisabledBuyButton = false;
     }
 }
