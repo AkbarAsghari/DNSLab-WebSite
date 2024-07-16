031e4525fd814acba55677247d3311cc8a02bb1b
Akbar <akbarasghari@outlook.com>
2024-07-16 16:11:17 +0330

    Fix No authenticationScheme was specified, and there was no DefaultChallengeScheme found Error on Referesh Page

---
+++ DNSLab/Middlewares/BlazorAuthorizationMiddlewareResultHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DNSLab.Middlewares
+{
+    public class BlazorAuthorizationMiddlewareResultHandler : IAuthorizationMiddlewareResultHandler
+    {
+        public Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
+        {
+            return next(context);
+        }
+    }
+}
