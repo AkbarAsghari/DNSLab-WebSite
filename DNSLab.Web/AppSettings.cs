391318a656447f7258c5d483870d13046c13ef32
Akbar <akbarasghari@outlook.com>
2025-01-06 23:32:40 +0330

    Make RecordTypes Settings

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace DNSLab.Web.AppSettings;
+﻿using DNSLab.Web.Enums;
+
+namespace DNSLab.Web.AppSettings;
 
 public sealed class GlobalSettings
 {
@@ -6,6 +8,23 @@ public sealed class GlobalSettings
     public const bool RightToLeft = true;
     public const bool DarkMode = true;
     public const string APIBaseAddress = "https://localhost:7046/";
+}
+
+public static class DNSSettings
+{
+    public static List<RecordTypeEnum> SupportedRecordForDomain = new List<RecordTypeEnum>
+    {
+        RecordTypeEnum.A,
+        RecordTypeEnum.AAAA,
+        RecordTypeEnum.CNAME,
+        RecordTypeEnum.TXT,
+        RecordTypeEnum.MX,
+    };
+
+    public static List<RecordTypeEnum> SupportedRecordForDDNS = new List<RecordTypeEnum>
+    {
+        RecordTypeEnum.A,
+    };
 }
 
 public sealed class AuthorizeRoles
