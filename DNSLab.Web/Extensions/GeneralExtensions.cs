3aff8fedbe02fc9e9033f0c02cf5450caf38ece1
Akbar <akbarasghari@outlook.com>
2025-01-14 14:10:11 +0330

    Fix Dashboard

--- DNSLab.Web/Extensions/GeneralExtensions.cs
+++ DNSLab.Web/Extensions/GeneralExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using DNSLab.Web.DTOs.Repositories.Record;
 using MD.PersianDateTime.Standard;
+using System.Text;
 using System.Text.Json;
 
 namespace DNSLab.Web.Extensions
@@ -43,5 +44,54 @@ namespace DNSLab.Web.Extensions
 
             return JsonSerializer.Serialize(jsonElement, options);
         }
+
+        static Dictionary<char, char> LettersDictionaryFaToEn = new Dictionary<char, char>() {
+            {'۰', '0'},
+            {'۱', '1'},
+            {'۲', '2'},
+            {'۳', '3'},
+            {'۴', '4'},
+            {'۵', '5'},
+            {'۶', '6'},
+            {'۷', '7'},
+            {'۸', '8'},
+            {'۹', '9'}
+        };
+
+        static Dictionary<char, char> LettersDictionaryEnToFa = new Dictionary<char, char>() {
+            {'0', '۰'},
+            {'1', '۱'},
+            {'2', '۲'},
+            {'3', '۳'},
+            {'4', '۴'},
+            {'5', '۵'},
+            {'6', '۶'},
+            {'7', '۷'},
+            {'8', '۸'},
+            {'9', '۹'}
+        };
+
+        public static string PersianToEnglishNumbers(this string input)
+        {
+            StringBuilder sb = new StringBuilder(input);
+            foreach (var item in input.Where(x => LettersDictionaryFaToEn.Keys.Contains(x)))
+                sb.Replace(item, LettersDictionaryFaToEn[item]);
+            return sb.ToString();
+        }
+
+        public static string EnglishToPersianNumbers(this string input)
+        {
+            StringBuilder sb = new StringBuilder(input);
+            foreach (var item in input.Where(x => LettersDictionaryEnToFa.Keys.Contains(x)))
+                sb.Replace(item, LettersDictionaryEnToFa[item]);
+            return sb.ToString();
+        }
+
+        public static string EnglishToPersianNumbers(this int? input)
+        {
+            if (input == null) return String.Empty;
+            return EnglishToPersianNumbers(input.Value.ToString());
+        }
+
     }
 }
