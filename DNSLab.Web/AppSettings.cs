2f1d44d9bd343855c50eb6805506b869c030c285
Akbar <akbarasghari@outlook.com>
2025-02-27 20:18:04 +0330

    Change VersionFix Home Page UI

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.0.4.0";
+    public const string Version = "1.0.4.1";
 
 #if DEBUG
     //public const string APIBaseAddress = "https://localhost:7046/";
